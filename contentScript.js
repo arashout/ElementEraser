1b2e8fe8faf5e966ffecbdadc77f86e1a00d06f6
arashout <arash.out@gmail.com>
2017-06-25 14:29:10 -0700

    Finally got a proof of concept working!

---
+++ contentScript.js
@@ -0,0 +1,19 @@
+'use strict';
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    removeDivs(message['classname'], message['searchTerms']);
+});
+
+function removeDivs(className, searchTerms){
+    var elements = document.getElementsByClassName(className);
+    for (let i = elements.length - 1; 0 <= i; i--) {
+        let element = elements[i];
+        let stringContent = element.textContent.toLowerCase();
+        if (searchTerms.some(function (term) {
+                return stringContent.includes(term);
+            })) {
+            console.log(element.textContent);
+            element.remove();
+        }
+    }
+}
--- popup.js
+++ popup.js
@@ -1,6 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('buttonDivsBeGone').addEventListener('click', buttonHandler);
+document.addEventListener("DOMContentLoaded", function () {
+
+    var query = {
+        active: true,
+        currentWindow: true
+    };
+
+    chrome.tabs.query(query, callbackCurrentTab);
 });
-function buttonHandler(){
-  alert("Button clicked");
+
+function callbackCurrentTab(tabs) {
+    document.getElementById('buttonDivsBeGone').onclick = function () {
+        var textSearchTerms = document.getElementById('textInputSearchTerms').value;
+        var listSearchTerms = textSearchTerms.split(',');
+
+        var popupDict = {
+            searchTerms: listSearchTerms,
+            classname: document.getElementById('textInputDivClass').value
+        }
+        chrome.tabs.sendMessage(tabs[0].id, popupDict, function (response) {
+
+        });
+
+    }
 }
