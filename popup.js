658b3e0d4bba65c0ef72d44520de86c11d68cd63
Arash Outadi <arash.out@gmail.com>
2017-06-26 19:47:04 -0700

    Making it easier to add list items

--- popup.js
+++ popup.js
@@ -5,21 +5,59 @@ document.addEventListener("DOMContentLoaded", function () {
         currentWindow: true
     };
 
-    chrome.tabs.query(query, callbackCurrentTab);
+    function sendPopupDict(tabs) {
+        document.getElementById('buttonDivErase').onclick = function () {
+            var nodeListSearchTerms = document.getElementById("listSearchTerms").getElementsByTagName("li");
+            var arrSearchTerms = getArrayFromList(nodeListSearchTerms);
+
+            var popupDict = {
+                searchTerms: arrSearchTerms,
+                classname: document.getElementById('inputDivClass').value
+            }
+            chrome.tabs.sendMessage(tabs[0].id, popupDict, function (response) {
+                console.log(response);
+            });
+
+        }
+    }
+
+    chrome.tabs.query(query, sendPopupDict);
+
+    function addListItemFromInput(ul, inputBox){
+            let text = inputBox.value;
+            let li = document.createElement("li");
+            li.classList.add('searchTerm');
+            li.appendChild(document.createTextNode(text.slice(0, text.length - 1)));
+            ul.appendChild(li);
+            inputBox.value = "";
+    }
+    
+    var ulSearchTerms = document.getElementById('listSearchTerms');
+    // Add all event listeners
+    var searchTermInputElement = document.getElementById('inputSearchTerm');
+    searchTermInputElement.addEventListener('input', function (e) {
+        if (this.value.endsWith(',')) {
+            addListItemFromInput(ulSearchTerms, this);
+        }
+    });
+    var ENTER_KEY_CODE = 13;
+    var TAB_KEY_CODE = 9;
+    searchTermInputElement.addEventListener('keydown', function (e){
+        if(this.value !== "" && (e.keyCode == ENTER_KEY_CODE || e.keyCode == TAB_KEY_CODE)){
+            addListItemFromInput(ulSearchTerms, this);
+        }
+    });
 });
 
-function callbackCurrentTab(tabs) {
-    document.getElementById('buttonDivsBeGone').onclick = function () {
-        var textSearchTerms = document.getElementById('textInputSearchTerms').value;
-        var listSearchTerms = textSearchTerms.split(',');
-
-        var popupDict = {
-            searchTerms: listSearchTerms,
-            classname: document.getElementById('textInputDivClass').value
-        }
-        chrome.tabs.sendMessage(tabs[0].id, popupDict, function (response) {
-
-        });
-
+/**
+ * Given an element that contains <li> items
+ * Return an array of strings for each <li> item
+ */
+function getArrayFromList(elementContainer) {
+    var arrayLi = [];
+    for (let i = 0; i < elementContainer.length; i++) {
+        let li = elementContainer[i];
+        arrayLi.push(li.textContent);
     }
+    return arrayLi;
 }
